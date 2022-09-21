09439d9aeaae91399989a2ab4c9a6a476c4a6802
orent2018 <42716702+orent2018@users.noreply.github.com>
2022-09-21 23:13:14 +0300

    Update backend.tf
    Disable backend [ci skip]

--- k8s-infra/backend.tf
+++ k8s-infra/backend.tf
@@ -8,10 +8,10 @@ terraform {
 
   required_version = ">=0.14.0"
   
-  backend "s3" {
-    bucket = "Cardinal-state"
-    key    = "k8sinfra/terraform.tfstate"
-    region = "eu-west-3"
-   }
+#   backend "s3" {
+#     bucket = "Cardinal-state"
+#     key    = "k8sinfra/terraform.tfstate"
+#     region = "eu-west-3"
+#    }
 
 }
