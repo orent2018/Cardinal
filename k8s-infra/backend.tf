e5ea716e0c149f5bf703c08121585e968199c772
orent2018 <orent66@gmail.com>
2022-09-22 00:04:06 +0300

    updated syntax [ci skip]

--- k8s-infra/backend.tf
+++ k8s-infra/backend.tf
@@ -1,17 +1,17 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~>3.27"
     }
   }
 
   required_version = ">=0.14.0"
-  
-#   backend "s3" {
-#     bucket = "Cardinal-state"
-#     key    = "k8sinfra/terraform.tfstate"
-#     region = "eu-west-3"
-#    }
+
+  #   backend "s3" {
+  #     bucket = "Cardinal-state"
+  #     key    = "k8sinfra/terraform.tfstate"
+  #     region = "eu-west-3"
+  #    }
 
 }
