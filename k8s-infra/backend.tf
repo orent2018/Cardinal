21188f7aa5f17db96acc04b6033dbb146565d224
orent2018 <42716702+orent2018@users.noreply.github.com>
2022-09-21 14:18:28 +0300

    Update backend.tf
    Changed backend to use S3 bucket [ci skip]

--- k8s-infra/backend.tf
+++ k8s-infra/backend.tf
@@ -1,11 +1,17 @@
 terraform {
-  backend "remote" {
-    organization = "orent2020"
-
-    workspaces {
-      name = "CardinalDemo"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      Version = "~>3.27"
     }
   }
 
-  required_version = ">= 0.14.0"
+  required_version = ">=0.14.0"
+  
+  backend "s3" {
+    bucket = "Cardinal-state"
+    key    = "k8sinfra/terraform.tfstate"
+    region = "eu-west-3"
+   }
+
 }
