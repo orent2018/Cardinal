5d51eda6f6d2135e3de25205fc06f29096cbf5ff
orent2018 <orent66@gmail.com>
2022-09-22 13:09:19 +0300

    Updated and tested config [ci skip]

--- k8s-infra/eks.tf
+++ k8s-infra/eks.tf
@@ -9,19 +9,19 @@ module "eks" {
   cluster_name    = local.cluster_name
   cluster_version = "1.21"
 
-  subnet_ids      = [aws_subnet.CardinalPrvSubnet.id]
-  vpc_id          = aws_vpc.CardinalVPC.id
+  subnet_ids = [aws_subnet.CardinalPrvSubnet.id]
+  vpc_id     = aws_vpc.CardinalVPC.id
   eks_managed_node_groups = {
-        min_size     = 2
-        max_size     = 4
-        desired_size = 2
+    min_size     = 2
+    max_size     = 4
+    desired_size = 2
 
-        instance_types = ["t2.small"]
-        labels = {
-          Environment = "Demo"
-        }
-        tags = {
-          ExtraTag = "CardinalDemoWorkers"
-        }
+    instance_types = ["t2.small"]
+    labels = {
+      Environment = "Demo"
+    }
+    tags = {
+      ExtraTag = "CardinalDemoWorkers"
+    }
   }
 }
