20ab1cfbcfa6b19e0ad5a3320e009f81eaa0e846
orent2018 <orent66@gmail.com>
2022-09-24 14:24:37 +0300

    Adding Container Registry base [ci skip]

--- k8s-infra/vpc.tf
+++ k8s-infra/vpc.tf
@@ -2,43 +2,43 @@
 data "aws_availability_zones" "available" {}
 
 #Create the VPC
-resource "aws_vpc" "CardinalVPC" {             # Creating VPC here
-  cidr_block           = var.main_vpc_cidr     # Defining the CIDR block use 10.0.0.0/24 for demo
+resource "aws_vpc" "CardinalVPC" {         # Creating VPC here
+  cidr_block           = var.main_vpc_cidr # Defining the CIDR block use 10.0.0.0/24 for demo
   enable_dns_hostnames = true
   instance_tenancy     = "default"
-  tags {
+  tags = {
     Name = "CardinalVPC"
   }
 }
 # Create Internet Gateway and attach it to VPC
-resource "aws_internet_gateway" "IGW" {        # Creating the Internet Gateway
+resource "aws_internet_gateway" "IGW" { # Creating the Internet Gateway
   vpc_id = aws_vpc.CardinalVPC.id
 }
 # Create a Public Subnet.
-resource "aws_subnet" "CardinalPubSubnet" {    # Creating Public Subnet
+resource "aws_subnet" "CardinalPubSubnet" { # Creating Public Subnet
   vpc_id     = aws_vpc.CardinalVPC.id
   cidr_block = var.public_subnet # CIDR block of public subnets
 }
 # Create a Private Subnet 1                    # Creating Private Subnets
 resource "aws_subnet" "CardinalPrvSubnet1" {
   vpc_id            = aws_vpc.CardinalVPC.id
-  cidr_block        = var.private_subnet1      # CIDR block of private subnet1
-  availability_zone = "${data.aws_availability_zones.available.names[0]}"
-  tags {
+  cidr_block        = var.private_subnet1 # CIDR block of private subnet1
+  availability_zone = data.aws_availability_zones.available.names[0]
+  tags = {
     Name = "PrivateSubnet1"
   }
 }
 # Create a Private Subnet 2                   
 resource "aws_subnet" "CardinalPrvSubnet2" {
   vpc_id            = aws_vpc.CardinalVPC.id
-  cidr_block        = var.private_subnet2       # CIDR block of private subnet2
-  availability_zone = "${data.aws_availability_zones.available.names[1]}"
-  tags {
+  cidr_block        = var.private_subnet2 # CIDR block of private subnet2
+  availability_zone = data.aws_availability_zones.available.names[1]
+  tags = {
     Name = "PrivateSubnet2"
   }
 }
 # Route table for Public Subnet's
-resource "aws_route_table" "PublicRT" {         # Creating RT for Public Subnet
+resource "aws_route_table" "PublicRT" { # Creating RT for Public Subnet
   vpc_id = aws_vpc.CardinalVPC.id
   route {
     cidr_block = "0.0.0.0/0" # Traffic from Public Subnet reaches Internet via Internet Gateway
@@ -46,7 +46,7 @@ resource "aws_route_table" "PublicRT" {         # Creating RT for Public Subnet
   }
 }
 # Route table for Private Subnet's
-resource "aws_route_table" "PrivateRT" {         # Creating RT for Private Subnet
+resource "aws_route_table" "PrivateRT" { # Creating RT for Private Subnet
   vpc_id = aws_vpc.CardinalVPC.id
   route {
     cidr_block     = "0.0.0.0/0" # Traffic from Private Subnet reaches Internet via NAT Gateway
